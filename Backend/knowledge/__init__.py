69eda1a102a40d575e0efc3e4772e5917add7248
AltimaOak <adi144.yadav@gmail.com>
2026-01-15 14:30:45 +0530

    add the knowledge init

---
+++ Backend/knowledge/__init__.py
@@ -0,0 +1,5 @@
+from .api_sources import APISources
+from .general_retriever import GeneralRetriever
+
+from .syllabus_ingestion import ingest_syllabus
+from .syllabus_retriever import retrieve_syllabus_context
